9224abc1ca9f20059a52a34a12affb1af4b55848
xCovn <alguenther@yahoo.de>
2021-10-27 21:23:38 +0200

    checkout github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/xCovn.github.io-vue-18n-starter/' : '/',
     runtimeCompiler: true,
     chainWebpack: config => {
         config.module
