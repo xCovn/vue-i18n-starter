da593e59fee664687506b2972e5d8607c1349f40
xCovn <alguenther@yahoo.de>
2021-10-27 20:58:05 +0200

    checkout github pages

---
+++ js/chunk-2d0e9741.c1238c71.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e9741"],{"8e31":function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("transition",{attrs:{name:"component-fade",mode:"out-in"}},[t("router-view",{on:{setAppClasses:function(n){return e.$emit("setAppClasses",n)}}})],1)},o=[],r=t("2877"),u={},a=Object(r["a"])(u,s,o,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0e9741.c1238c71.js.map
